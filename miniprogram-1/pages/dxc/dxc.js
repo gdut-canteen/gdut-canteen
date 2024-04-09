cf5635b09960d1a1eac0710a778e533cb93212de
brokensc <965935569@qq.com>
2024-04-09 20:52:06 +0800

    食堂选择模块

--- miniprogram-1/pages/dxc/dxc.js
+++ miniprogram-1/pages/dxc/dxc.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    Lists:[//TODO:需要将stars的值进行计算
+      {id:1, imageSrc:"/images/canteens/fourth/一楼港式烧腊饭.jpg", name:"四饭一楼港式烧腊饭", stars:null},
+      {id:2, imageSrc:"/images/canteens/fourth/一楼滑蛋饭.jpg", name:"四饭一楼滑蛋饭", stars:null},
+      {id:3, imageSrc:"/images/canteens/fourth/一楼潮汕卤水饭.jpg", name:"四饭一楼潮汕卤水饭",stars:null},
+      {id:4, imageSrc:"/images/canteens/fourth/一楼瓦罐汤.jpg", name:"四饭一楼瓦罐汤", stars:null},
+      {id:5, imageSrc:"/images/canteens/fourth/一楼粉面世家.jpg", name:"四饭一楼粉面世家",stars:null},
+      {id:6, imageSrc:"/images/canteens/fourth/一楼自选.jpg", name:"四饭一楼自选", stars:null},
+      {id:7, imageSrc:"/images/canteens/fourth/二楼烤肉.jpg", name:"四饭二楼烤肉饭", stars:null},
+      {id:8, imageSrc:"/images/canteens/fourth/二楼煎肉饼.jpg", name:"四饭二楼牛牛煎肉饼", stars:null},
+      {id:9, imageSrc:"/images/canteens/fourth/二楼老时间.jpg", name:"四饭二楼老时间", stars: null},
+      {id:10, imageSrc:"/images/canteens/fourth/二楼莫老鸭.jpg", name:"四饭二楼莫老鸭", stars: null}
+    ],
   },
 
   /**
