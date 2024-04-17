bab88874e59af4bb735c213839a1c586d7376844
brokensc <965935569@qq.com>
2024-04-17 23:10:03 +0800

    失物招领 篮煜楷 4/17

--- miniprogram-1/pages/search/search.js
+++ miniprogram-1/pages/search/search.js
@@ -1,66 +1,93 @@
 // pages/search/search.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  /*页面的初始数据*/
   data: {
-
+    list: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  data:{
+    Lists:[]
+  },
+  staticData: {
+    inputValue: ""
+  },
+  lostrecord:function(){
+    wx.navigateTo({
+      url:'/pages/search/lostrecords',
+    })
+  },
+  /*生命周期函数--监听页面加载*/
   onLoad(options) {
-
+    this.getSearchResult("");
+    var Lists = wx.getStorageSync('lost-data')
+    wx.removeStorageSync('lost-data')
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+ getSearchResult(keyword) {
+  wx.request({
+       url: 'xxxxxx',
+       data: {
+         keyword: this.staticData.inputValue
+       },
+       method: "POST",
+       header: {
+         'content-type': 'application/x-www-form-urlencoded' 
+       },
+       success: this.getSearchResultSucc.bind(this)
+  })
+  },
+  getSearchResultSucc(res) {
+    // console.log(res)
+    if (res.data.ret) {
+      const result = res.data.data;
+      this.setData({
+        list: result
+      })
+    } else {
+      this.setData({
+        list: []
+      })
+    }
+  },
+  handleInputChange(e) {
+    this.staticData.inputValue = list.detail.value;
+  },
+  handleSearch (keyword) {
+    this.getSearchResult(keyword)
+  },
+  handleItemTap(e) {
+      wx.navigateTo({
+        url: '/pages/detail/detail?id=' + lost.currentTaret.id
+      })
+  },
+  /*生命周期函数--监听页面初次渲染完成*/
   onReady() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  /*生命周期函数--监听页面显示*/
   onShow() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+  /*生命周期函数--监听页面隐藏*/
   onHide() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+  /*生命周期函数--监听页面卸载*/
   onUnload() {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  /*页面相关事件处理函数--监听用户下拉动作*/
   onPullDownRefresh() {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+  /*页面上拉触底事件的处理函数*/
   onReachBottom() {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
+  /*用户点击右上角分享*/
   onShareAppMessage() {
 
   }
 })
+
+const App = getApp()
